c2570d620b9f871ac6690f519e8a648b5883fb43
4 <4>
2025-01-31 11:06:37 +0530

    scprt edt

--- scripts/g.sh
+++ scripts/g.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-toilet="toilet -t --metal --font future"
+toilet="toilet -t --metal --font pagga"
 
 cd ~/4end
-echo "GIT COMMIT SCRIPT STARTED" | $toilet
+echo "GIT COMMIT STARTED" | $toilet
 git add .
 read -p "enter commit message: " msg
 git commit -m "$msg"
 git push origin main
-echo "GIT COMMIT SCRIPT ENDED" | $toilet
+echo "GIT COMMIT ENDED" | $toilet
