70ec290452b7957e21d50c9df47889f1e483f5da
4 <4>
2025-03-19 20:59:35 +0530

    rm usles fldr

--- scripts/1.sh
+++ scripts/1.sh
@@ -41,7 +41,6 @@ echo "FISH test incoming..." && fish ignorethiserrormone
 
 read -p "copy/overwrite DOTFILES..? " das
 if [[ $das = y ]]; then
-  mkdir ~/puthiyam && cp -r ~/.config ~/puthiyam
   cp -r ~/4arch/confs/fuzzel ~/.config
   cp -r ~/4arch/confs/hypr ~/.config
   cp -r ~/4arch/confs/kitty ~/.config
