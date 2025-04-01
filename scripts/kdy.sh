4f79b4fb7f959306625f52c8f6fbebcc21c3db0f
4 <4>
2025-04-01 07:51:48 +0530

    kdy shyt

--- scripts/kdy.sh
+++ scripts/kdy.sh
@@ -6,12 +6,15 @@ N="sudo nvim"
 W="wl-copy -n"
 Y="yay -S --needed --noconfirm"
 
-echo "Installing critical apps..."
-sudo pacman -Syu
-$PS fish wl-clipboard inkscape fastfetch htop btop kvantum
-$PS power-profiles-daemon
-
-sudo systemctl enable --now power-profiles-daemon
+read -p "install YAY - Yet Another AUR Helper..? " yas
+if [[ $yas = y ]]; then
+  rm -rf ~/yay-bin
+  sudo pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay-bin.git
+  cd yay-bin && makepkg -si && cd && yay
+else
+  echo "skipped YAY setup..!"
+fi
 
 read -p "configure CHAOTIC-AUR repo..? " cas
 if [[ $cas = y ]]; then
@@ -29,43 +32,7 @@ if [[ $ras = y ]]; then
   reboot
 fi
 
-read -p "install YAY - Yet Another AUR Helper..? " yas
-if [[ $yas = y ]]; then
-  rm -rf ~/yay-bin
-  sudo pacman -S --needed git base-devel
-  git clone https://aur.archlinux.org/yay-bin.git
-  cd yay-bin && makepkg -si && cd && yay
-else
-  echo "skipped YAY setup..!"
-fi
-
-read -p "sudoedit SYSTEM-files..? " sas
-if [[ $sas = y ]]; then
-  $W < ~/4arch/scripts/scriptiles/chaoty.sh
-  $N /etc/pacman.conf && yay
-
-  $W HandlePowerKey=suspend-then-hibernate
-  $N /etc/systemd/logind.conf
-
-  $W HibernateDelaySec=2400s
-  $N /etc/systemd/sleep.conf
-
-  $W "/swapfile none swap defaults 0 0"
-  $N /etc/fstab
-
-  $W resume
-  $N /etc/mkinitcpio.conf
-else
-  echo "skipped SYSTEM-files editing..!"
-fi
-
-read -p "configure SWAP-file..? " was
-if [[ $was = y ]]; then
-  sudo mkswap -U clear --size 8G --file /swapfile
-  sudo swapon /swapfile
-else
-  echo "skipped SWAP-file creation..!"
-fi
+$N /etc/pacman.conf
 
 read -p "configure FISH as interactive shell..? " fas
 if [[ $fas = y ]]; then
@@ -75,32 +42,41 @@ else
   echo "skipped bafish configuration..!"
 fi
 
+read -p "sudoedit SYSTEM-files..? " sas
+if [[ $sas = y ]]; then
+
+  $W HandlePowerKey=suspend-then-hibernate
+  $N /etc/systemd/logind.conf
+
+  $W HibernateDelaySec=2400
+  $N /etc/systemd/sleep.conf
+
+  $W resume
+  $N /etc/mkinitcpio.conf
+else
+  echo "skipped SYSTEM-files editing..!"
+fi
+
 read -p "regenerate INITRAMFS..? " ias
 if [[ $ias = y ]]; then
   sudo mkinitcpio -P
 else
   echo "skipped INITRAMFS regeneration..!"
 fi
-
+ 
+$Y plasma-meta konsole ark dolphin nautilus
 $Y noto-fonts noto-fonts-cjk noto-fonts-extra
-$Y noto-fonts-emoji ttf-jetbrains-mono-nerd
+$Y noto-fonts-emoji ttf-jetbrains-mono-nerd ttf-fira-sans
 $Y brave telegram-desktop qbittorrent gnome-boxes
-$Y git-credential-manager-bin
+$Y git-credential-manager-bin inkscape
+$Y lutris goverlay lib32-mangohud wine-mono
+$Y lib32-vulkan-dzn lib32-vulkan-gfxstream lib32-vulkan-icd-loader
+$Y lib32-vulkan-intel lib32-vulkan-mesa-layers lib32-vulkan-swrast
+$Y lib32-vulkan-utility-libraries lib32-vulkan-validation-layers
+$Y lib32-vulkan-virtio memtest_vulkan vulkan-dzn vulkan-extra-layers
+$Y vulkan-extra-tools vulkan-gfxstream vulkan-headers vulkan-icd-loader
+$Y vulkan-intel vulkan-mesa-layers vulkan-swrast vulkan-tools
+$Y vulkan-utility-libraries vulkan-validation-layers vulkan-virtio
 
-echo "Downloading Kvantum theme..."
-brave "https://www.pling.com/find?search=sweet"
-dolphin
-kvantummanager
-systemsettings
-konsole
-
-cp -r /home/archy/.local/share/plasma/desktoptheme/Sweet ~/sweeThemebackup
-inkscape /home/archy/.local/share/plasma/desktoptheme/Sweet/widgets/panel-background.svgz
-systemctl --user restart plasma-plasmashell.service
-
-read -p "finished script execution, REBOOT now..? " ras
-if [[ $ras = y ]]; then
-  reboot
-else
-  echo "please REBOOT manually..!"
-fi
+systemctl enable sddm.service
+systemctl enable power-profiles-daemon.service
