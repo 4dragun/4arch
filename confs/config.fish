630570d23817ae838d3ad5c7b4771fa17c8bc972
4 <4>
2025-04-04 21:10:13 +0530

    fishconf

--- confs/config.fish
+++ confs/config.fish
@@ -5,7 +5,6 @@ end
 alias y="yazi"
 alias ls="lsd"
 alias cat="bat"
-alias c="~/4arch/scripts/c.sh"
 alias gi="~/4arch/scripts/gi.sh"
 alias gc="~/4arch/scripts/gc.sh"
 alias gp="~/4arch/scripts/gp.sh"
