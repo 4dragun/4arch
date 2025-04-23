0d422603f2bafc5306c144d5e7d743e3c8c2a8f8
4 <4>
2025-04-23 12:47:49 +0530

    fishs edt

--- confs/fish/config.fish
+++ confs/fish/config.fish
@@ -7,16 +7,16 @@ function prompt_newline --on-event fish_postexec
 	echo
 end
 
-alias clear "command clear; commandline -f clear-screen"
 if status is-interactive
   set -U fish_greeting
 end
 
-alias y="yazi"
-alias ls="lsd"
-alias cat="bat"
-alias gi="~/4arch/scripts/gi.sh"
-alias gc="~/4arch/scripts/gc.sh"
-alias gp="~/4arch/scripts/gp.sh"
+alias y     "yazi"
+alias ls    "lsd"
+alias cat   "bat"
+alias gi    "~/4arch/scripts/gi.sh"
+alias gc    "~/4arch/scripts/gc.sh"
+alias gp    "~/4arch/scripts/gp.sh"
+alias clear "command clear; commandline -f clear-screen"
 
 starship init fish | source
