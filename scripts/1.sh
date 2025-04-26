8fece72198ba96cad235c1cbbf35ba3411f631d2
4 <4>
2025-04-26 08:33:58 +0530

    scpr1 editedd

--- scripts/1.sh
+++ scripts/1.sh
@@ -75,8 +75,8 @@ echo "installing DEPENDENCIES..."
 $YS ffmpegthumbnailer python-pillow bibata-cursor-theme tumbler gvfs gvfs-mtp
 
 echo "installing FONTS..."
-$YS noto-fonts noto-fonts-cjk noto-fonts-extra otf-font-awesome
-$YS noto-fonts-emoji otf-comicshanns-nerd
+$YS noto-fonts noto-fonts-cjk noto-fonts-extra ttf-font-awesome
+$YS noto-fonts-emoji ttf-jetbrains-mono-nerd ttf-rubik-vf
 
 echo "installing HYPRLAND-stuff..."
 $YS hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk uwsm kitty
