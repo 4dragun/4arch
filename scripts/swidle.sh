d79916085c7c3c92b3c08a606fec0b378dc6290b
4 <4>
2025-05-03 17:21:18 +0530

    swidle scpt time

--- scripts/swidle.sh
+++ scripts/swidle.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 killall swayidle
 swayidle -w \
-  timeout 10 'swaylock -f' \
-  timeout 12 'brightnessctl -rs && niri msg output eDP-1 off' \
+  timeout 300 'swaylock -f' \
+  timeout 420 'brightnessctl -rs && niri msg output eDP-1 off' \
   resume 'brightnessctl -r && niri msg output eDP-1 on' \
   before-sleep 'brightnessctl -rs && swaylock -f'\
-  timeout 16 'brightnessctl -rs && systemctl hibernate'
+  timeout 600 'brightnessctl -rs && systemctl hibernate'
