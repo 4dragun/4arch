3202093e94f53f84d1ec3d099189e649e52f614b
4 <4>
2025-06-30 20:41:47 +0530

    main1script edit

--- scripts/1.sh
+++ scripts/1.sh
@@ -53,7 +53,7 @@ if [[ $yas = y ]]; then
   rm -rf ~/yay-bin
   sudo pacman -S --needed --noconfirm git base-devel
   git clone https://aur.archlinux.org/yay-bin.git
-  cd yay-bin && makepkg -si && cd && yay
+  cd yay-bin && makepkg -si --noconfirm && cd && yay --noconfirm
 else
   echo "skipped YAY setup..!"
 fi
