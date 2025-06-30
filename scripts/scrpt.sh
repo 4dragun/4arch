1f33e65f660cc1b7caa1b11c414ab8eb2e3d2706
4 <4>
2025-06-30 23:33:37 +0530

    edit scrpt.sh

--- scripts/scrpt.sh
+++ scripts/scrpt.sh
@@ -2,6 +2,6 @@
 
 read -p "Enter SCRIPT name: " sas
 
-printf '#!/usr/bin/env bash\n\n\n' > "$sas"
-chmod +x $sas
-nvim +3 $sas
+printf '#!/usr/bin/env bash\n\n\n' > "$sas.sh"
+chmod +x $sas.sh
+nvim +3 $sas.sh
