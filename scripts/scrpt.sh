5a467668e2187b643714b0adb1c2873d2cbea513
4 <4>
2025-07-03 15:10:40 +0530

    enhanced scrpt.sh

--- scripts/scrpt.sh
+++ scripts/scrpt.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-echo
-echo "󰲋 CREATING EXECUTABLE SCRIPT"
-echo
+echo && echo "󰲋 CREATING EXECUTABLE SCRIPT" && echo
 
 read -p "Enter SCRIPT name: " sas
+sas="${sas%.sh}"
 printf '#!/usr/bin/env bash\n\n\n' > "$sas.sh"
-chmod +x $sas.sh
+chmod +x "$sas.sh"
 
-nvim +3 $sas.sh
+nvim +3 "$sas.sh"
