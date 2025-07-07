1ac06ef518c77b723f797cada35788a3572d801e
4 <4>
2025-07-07 08:36:33 +0530

    bsta.sh edit

--- scripts/bsta.sh
+++ scripts/bsta.sh
@@ -7,8 +7,8 @@ systemctl start "$SN"
 SS="$(systemctl is-active "$SN")"
 
 if [ "$SS" = "active" ]; then
-  notify-send -i "~/4arch/azzets/bactive.png" "Bluetooth" "Bluetooth service is active"
+  notify-send -i "$HOME/4arch/azzets/bactive.png" "Bluetooth" "Bluetooth service is active"
   blueman-manager
 else
-  notify-send -i "~/4arch/azzets/binactive.png" "Bluetooth" "Bluetooth service is inactive"
+  notify-send -i "$HOME/4arch/azzets/binactive.png" "Bluetooth" "Bluetooth service is inactive"
 fi
