2d968f9f9c7c2e7ca70e25bebb45adea371d50aa
4 <4>
2025-08-10 14:18:37 +0530

    wybar, file renames, todo update

--- todotodotodo.nix
+++ todotodotodo.nix
@@ -2,13 +2,6 @@ Wierd USB interacting with hibernate fix {
 
   * TRY ENDEAVOURS OS maybe......
 
-  check arch linux default pacman "CHECKSPACE option ..?"
-
-  GTK 2 apps not using icon, TRYING DCONF FOLDER FOR NOW
-
-instead of copying pacman, mkini, etc.. why not the whole foldr ??
-
-
 -----PIANO LESSSONS ........ ??????
 
 ---------SDDM THEME ............. ?
