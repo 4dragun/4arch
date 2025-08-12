c81abcda3fd78d75b10614b5172a2ec889784637
4 <4>
2025-08-12 14:28:44 +0530

    main1, todo update

--- scripts/1.sh
+++ scripts/1.sh
@@ -8,10 +8,10 @@ ZSF="$HOME/.config/ZORIGINAL_SYSTEM_FILES"
 WALLDIR="$HOME/4arch/walls/wallhaven-7jgyre_1920x1080.png"
 
 echo
-echo " ... WELCOME to 4ARCH Script ... "
+echo " ... WELCOME to 4ARCH Script ..."
 echo
 
-echo " >>> running PACMAN-KEY before proceeding >>> "
+echo " >>> running PACMAN-KEY before proceeding >>>"
 sudo pacman-key --init
 echo
 sudo pacman-key --populate archlinux
@@ -32,7 +32,7 @@ if [[ "$itd" = y ]]; then
   rm -rf ~/.local/state/nvim
   rm -rf ~/.local/share/nvim
   echo
-  echo " === cloning NvChad === "
+  echo " === cloning NvChad ==="
   echo
   git clone https://github.com/NvChad/starter ~/.config/nvim
   echo
@@ -42,14 +42,14 @@ if [[ "$itd" = y ]]; then
   echo
 else
   echo
-  echo " ___ skipped DOTFILES, ICONS, THEMES setup ___ "
+  echo " ___ skipped DOTFILES, ICONS, THEMES setup ___"
 fi
 
 if [[ "$yas" = y ]]; then
   echo
   sudo pacman -S --needed --noconfirm git base-devel
   echo
-  echo " === cloning YAY === "
+  echo " === cloning YAY ==="
   echo
   rm -rf ~/yay-bin
   echo
@@ -59,12 +59,12 @@ if [[ "$yas" = y ]]; then
   echo
 else
   echo
-  echo " ___ skipped YAY setup ___ "
+  echo " ___ skipped YAY setup ___"
 fi
 
 if [[ "$cas" = y ]]; then
   echo
-  echo " ... adding CHAOTIC-AUR repo ... "
+  echo " ... adding CHAOTIC-AUR repo ..."
   echo
   sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
   echo
@@ -76,7 +76,7 @@ if [[ "$cas" = y ]]; then
   echo
 else
   echo
-  echo " ___ skipped CHAOTIC-AUR setup ___ "
+  echo " ___ skipped CHAOTIC-AUR setup ___"
   echo
 fi
 
@@ -86,7 +86,7 @@ if [[ "$ras" = y ]]; then
   sync && sync && sync && systemctl reboot
 else
   echo
-  echo " ~~~ CHAOTIC-AUR added SUCCESSFULLY ~~~ "
+  echo " ~~~ CHAOTIC-AUR added SUCCESSFULLY ~~~"
   echo
 fi
 
@@ -97,15 +97,15 @@ if [[ "$cop" = y ]]; then
   if [[ "$pas" = archydoes ]]; then
     if [[ -d "$ZSF" ]]; then
       echo
-      echo " >>> BACKED-UP FOLDER ALREADY EXISTS ..! skipping backup <<< "
+      echo " >>> BACKED-UP FOLDER ALREADY EXISTS ..! skipping backup <<<"
       echo
     else
       echo
-      echo " >>> creating ZORIGINAL_SYSTEM_FILES folder ... "
+      echo " >>> creating ZORIGINAL_SYSTEM_FILES folder ..."
       echo
       sudo mkdir -p "$ZSF"
       echo
-      echo " >>> backing-up files now ... "
+      echo " >>> backing-up files now ..."
       echo
       sudo cp -r /etc/systemd/logind.conf "$ZSF"
       sudo cp -r /etc/systemd/sleep.conf "$ZSF"
@@ -113,18 +113,18 @@ if [[ "$cop" = y ]]; then
       sudo cp -r /etc/pacman.conf "$ZSF"
       sudo chattr +i "$ZSF"
       echo
-      echo " <<< ORIGINAL_SYSTEM_FILES Backup Complete >>> "
+      echo " <<< ORIGINAL_SYSTEM_FILES Backup Complete >>>"
       echo
     fi
   else
     echo
-    echo " ___ wrong pass dude :( ___ "
+    echo " ___ wrong pass dude :( ___"
     echo
     exit
   fi
 else
   echo
-  echo " ___ skipped BACKUP of original system files ___ "
+  echo " ___ skipped BACKUP of original system files ___"
   echo
 fi
 
@@ -132,19 +132,20 @@ read -p " -> replace SYSTEM_FILES with custom ones ? (y/n) = " sas
 if [[ "$sas" = y ]]; then
   if [[ -d "$ZSF" ]]; then
     echo
-    echo " --- backup folder found --- "
-    echo " --- proceeding --- "
+    echo " --- backup folder found ---"
+    echo " --- proceeding ---"
     echo
     sudo cp -r 4arch/confs2/. /etc
   else
     echo
-    echo " --- backup folder was not found --- "
-    echo " --- cannot procede --- "
+    echo " --- backup folder was not found ---"
+    echo " --- cannot procede ---"
     echo
+    exit
   fi
 else
   echo
-  echo " ___ skipped replacing SYSTEM_FILES with custom ones ___ "
+  echo " ___ skipped replacing SYSTEM_FILES with custom ones ___"
   echo
 fi
 
@@ -155,11 +156,11 @@ if [[ "$mas" = y ]]; then
   echo
 else
   echo
-  echo " ___ skipped MKINITCPIO -P ___ "
+  echo " ___ skipped MKINITCPIO -P ___"
   echo
 fi
 
-echo " >>> running YAY to update >>> "
+echo " >>> running YAY to update >>>"
 echo
 yay --noconfirm
 echo
@@ -168,28 +169,28 @@ echo "installing AUR-apps..."
 $YS clipse-bin matugen-bin
 echo
 echo "installing DEPENDENCIES..."
-$YS bibata-cursor-theme adw-gtk-theme darkly-qt6-git
-$YS papirus-icon-theme papirus-folders
-$YS lua-language-server
+$YS bibata-cursor-theme adw-gtk-theme darkly-qt6-git \
+    papirus-icon-theme papirus-folders \
+    lua-language-server
 echo
 echo "installing FONTS..."
-$YS noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
-$YS ttf-jetbrains-mono-nerd
+$YS noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji \
+    ttf-jetbrains-mono-nerd
 echo
 echo "installing HYPRLAND-stuff..."
-$YS hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-kde grimblast
-$YS qt5-wayland hypridle hyprlock hyprpicker hyprpolkitagent hyprpaper
+$YS hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-kde grimblast \
+    qt5-wayland hypridle hyprlock hyprpicker hyprpolkitagent hyprpaper
 echo
 echo "installing GUI-apps..."
-$YS sddm brave emote pavucontrol telegram-desktop mpv gwenview rofi-wayland
-$YS firefox nwg-look blueman qbittorrent swaync reflector-simple neovide
-$YS waybar nwg-look network-manager-applet dolphin ark swappy
-$YS kdialog systemsettings
+$YS sddm brave emote pavucontrol telegram-desktop mpv gwenview rofi-wayland \
+    firefox nwg-look blueman qbittorrent swaync reflector-simple neovide \
+    waybar nwg-look network-manager-applet dolphin ark swappy \
+    kdialog systemsettings
 echo
 echo "installing CLI-apps..."
-$YS fzf lsd bat pacseek fastfetch btop udiskie kitty aria2
-$YS git-credential-manager-bin yazi wl-clipboard brightnessctl starship
-$YS power-profiles-daemon xdg-user-dirs
+$YS fzf lsd bat pacseek fastfetch btop udiskie kitty aria2 \
+    git-credential-manager-bin yazi wl-clipboard brightnessctl starship \
+    power-profiles-daemon xdg-user-dirs
 echo
 
 xdg-user-dirs-update
@@ -199,7 +200,7 @@ echo
 # kbuildsycoca6
 echo
 
-echo " --> reached MATUGEN color generation area <-- "
+echo " -> reached MATUGEN color generation area <-"
 echo
 matugen image "$WALLDIR"
 echo
@@ -207,11 +208,9 @@ echo "$WALLDIR" > "$HOME/.cache/last-wall.txt"
 echo
 papirus-folders -C violet
 
-echo " ... enabling POWER-PROFILES-DAEMON ... "
-sudo systemctl enable power-profiles-daemon
-echo
-echo " ... enabling SDDM - Display Manager ... "
-sudo systemctl enable sddm
+echo " ... enabling SERVICES ..."
+sudo systemctl enable power-profiles-daemon \
+                      sddm
 echo
 
 read -p " ... 4ARCH Script Ended. REBOOT now ? (y/n) = " nas
@@ -220,6 +219,6 @@ if [[ "$nas" = y ]]; then
   sync && sync && sync && systemctl reboot
 else
   echo
-  echo " === Alrighty, Reboot manually === "
+  echo " === Alrighty then, Reboot manually ==="
   echo
 fi
--- todotodotodo.nix
+++ todotodotodo.nix
@@ -2,6 +2,10 @@ Wierd USB interacting with hibernate fix {
 
   * TRY ENDEAVOURS OS maybe......
 
+INSTAED OF $YS just do \\\\\\
+same for systemctl ???
+
+
 -----PIANO LESSSONS ........ ??????
 
 ---------SDDM THEME ............. ?
