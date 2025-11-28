659fad1216b9f371d931387bc3b44cbef0e1236d
4 <4>
2025-11-28 20:30:49 +0530

    edit waybar conf, amixerbar.sh edit

--- scripts/internal/amixerbar.sh
+++ scripts/internal/amixerbar.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-out=$(amixer -M -c 1 get "Speaker Volume")
+while true; do
+  out=$(amixer -M -c 1 get "Speaker Volume")
+  
+  L=$(echo "$out" | awk -F'[][]' '/Front Left:/  {gsub("%","",$2); print $2}')
+  R=$(echo "$out" | awk -F'[][]' '/Front Right:/ {gsub("%","",$2); print $2}')
 
-# Extract percentage numbers without the % sign
-L=$(echo "$out" | awk -F'[][]' '/Front Left:/  {gsub("%","",$2); print $2}')
-R=$(echo "$out" | awk -F'[][]' '/Front Right:/ {gsub("%","",$2); print $2}')
+  echo "    $L  $R"
 
-echo "    $L  $R"
+  sleep 0.5
+done
