82d5d5525d3c38ffb6a40e4cb4beeb1fb88fac72
4 <4>
2025-11-28 12:45:45 +0530

    amixerbar.sh edit

--- scripts/internal/amixerbar.sh
+++ scripts/internal/amixerbar.sh
@@ -6,4 +6,4 @@ out=$(amixer -M -c 1 get "Speaker Volume")
 L=$(echo "$out" | awk -F'[][]' '/Front Left:/  {gsub("%","",$2); print $2}')
 R=$(echo "$out" | awk -F'[][]' '/Front Right:/ {gsub("%","",$2); print $2}')
 
-echo "   $L  $R"
+echo "    $L  $R"
