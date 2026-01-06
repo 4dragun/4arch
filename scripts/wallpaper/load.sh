0982482fc8d7eb68d6ccc061b2ca2545b940ba39
4 <4>
2026-01-06 22:28:33 +0530

    hyprpaper fixes

--- scripts/wallpaper/load.sh
+++ scripts/wallpaper/load.sh
@@ -10,4 +10,4 @@ if [[ -z "$WALL" || ! -f "$WALL" ]]; then
   echo -e "\n~ NO SAVED WALLPAPER FOUND OR FILE MISSING!\n"; exit
 fi
 
-hyprctl hyprpaper reload ,"$WALL"
+hyprctl hyprpaper wallpaper ",$WALL"
