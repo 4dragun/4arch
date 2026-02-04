a6c6be358fa8d4cb9ea35b75195601a7ff60110c
4 <4>
2026-02-04 22:26:25 +0530

    edit amixerbar.sh

--- scripts/internal/amixerbar.sh
+++ scripts/internal/amixerbar.sh
@@ -3,7 +3,7 @@
 while true; do
   out=$(amixer -M -c 1 get "PCM" 2>/dev/null)
   
-  [[ -z "$out" ]] && echo "󱥔 󰿦  " && sleep 0.5 && continue
+  [[ -z "$out" ]] && echo "󱥔   " && sleep 0.5 && continue
   
   L=$(awk -F'[][]' '/Front Left:/ {gsub("%","",$2); print $2}' <<< "$out")
   R=$(awk -F'[][]' '/Front Right:/ {gsub("%","",$2); print $2}' <<< "$out")
