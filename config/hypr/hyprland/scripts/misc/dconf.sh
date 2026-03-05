6b04b14b2b25c866e382c4c97c33d8a99bcecff1
4 <4>
2026-03-05 23:40:33 +0530

    MASSIVE

--- config/hypr/hyprland/scripts/misc/dconf.sh
+++ config/hypr/hyprland/scripts/misc/dconf.sh
@@ -2,6 +2,6 @@
 
 D="dbus-run-session gsettings set org.gnome.desktop.interface"
 
-$D gtk-theme 'adw-gtk3-dark'
+$D gtk-theme  'adw-gtk3-dark'
 $D icon-theme 'matutela'
-$D font-name 'Rubik 12'
+$D font-name  'Rubik 12'
