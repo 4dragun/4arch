903d1ab6c148f5dafcebc58d67cfd6d78fbd5e04
4 <4>
2026-03-19 08:17:49 +0530

    edit 02

--- install/02_desktop.sh
+++ install/02_desktop.sh
@@ -58,12 +58,12 @@ while true; do
 done
 
 while true; do
-  read -p "===> EXP. INSTALL PARU? (y/n) = " pas; echo; pas="${pas,,}"
+  read -p "===> INSTALL PARU? (y/n) = " pas; echo; pas="${pas,,}"
 
   if [[ "$pas" == "y" ]]; then
 
     while true; do
-      clear; echo -e "\n>>>> EXP. INSTALLING PARU...\n"
+      clear; echo -e "\n>>>> INSTALLING PARU...\n"
 
       if sudo pacman -S --needed --noconfirm paru; then
         clear; echo -e "\n>>>> SUCCESS: installed PARU!\n"; break 2
@@ -78,7 +78,8 @@ while true; do
           if [[ "$pras" == "y" ]]; then
             break
           elif [[ "$pras" == "n" ]]; then
-            clear; echo -e "\n>>>> ABORT: cancelled PARU installation!\n"; break 3
+            clear; echo -e "\n>>>> ABORT: cancelled PARU installation!\n"
+            break 3
           else
             clear; echo -e "\n$ERRMSG\n"
           fi
@@ -100,10 +101,7 @@ while true; do
 
     while true; do
       
-      if clear && echo -e "\n>>>> INSTALLING AUR PACKAGES...\n" &&\
-        $YS ttf-rubik-vf wvkbd ayugram-desktop-bin surge-bin darkly-bin &&\
-
-        echo -e "\n>>>> INSTALLING INTERNAL DEPENDENCIES...\n" &&\
+      if clear && echo -e "\n>>>> INSTALLING INTERNAL DEPENDENCIES...\n" &&\
         $YS bibata-cursor-theme adw-gtk-theme lua-language-server\
             gst-plugins-bad xdg-user-dirs archlinux-xdg-menu\
             libadwaita-without-adwaita-git &&\
@@ -113,19 +111,23 @@ while true; do
             ttf-jetbrains-mono-nerd &&\
 
         echo -e "\n>>>> INSTALLING HYPRLAND AND ITS DEPENDENCIES...\n" &&\
-        $YS hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-kde grimblast\
-            qt5-wayland hypridle hyprlock hyprpicker hyprpolkitagent hyprpaper &&\
+        $YS hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-kde\
+            grimblast qt5-wayland hypridle hyprlock hyprpicker hyprpolkitagent\
+            hyprpaper &&\
 
         echo -e "\n>>>> INSTALLING GUI APPLICATIONS...\n" &&\
         $YS sddm brave emote pavucontrol-qt gwenview rofi-wayland\
             nwg-look blueman qbittorrent swaync reflector-simple neovide mpv\
-            waybar network-manager-applet dolphin swappy systemsettings kdialog\
-            p7zip-gui zen-browser-bin strawberry &&\
+            waybar network-manager-applet dolphin swappy systemsettings\
+            kdialog p7zip-gui zen-browser-bin strawberry &&\
 
         echo -e "\n>>>> INSTALLING CLI APPLICATIONS...\n" &&\
         $YS fzf lsd bat pacseek fastfetch btop udiskie kitty yazi starship\
             git-credential-manager-bin wl-clipboard brightnessctl alsa-utils\
-            power-profiles-daemon clipse matugen; then
+            power-profiles-daemon clipse matugen &&\
+
+        echo -e "\n>>>> INSTALLING AUR PACKAGES...\n" &&\
+        $YS ttf-rubik-vf wvkbd ayugram-desktop-bin surge-bin darkly-bin; then
 
         clear; echo -e "\n>>>> SUCCESS: finished installing APPS & UTILS!\n"
         break 2
@@ -140,7 +142,7 @@ while true; do
           if [[ "$rias" == "y" ]]; then
             break
           elif [[ "$rias" == "n" ]]; then
-            clear; echo -e "\n>>>> ABORT: cancelled APPS & UTILS installation!\n"
+            clear; echo -e "\n>>>> ABORT: aborted APPS & UTILS installation!\n"
             break 3
           else
             clear; echo -e "\n$ERRMSG\n"
